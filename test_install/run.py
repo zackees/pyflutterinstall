179381d0841c8653b38c3f63a210edaf81fe7aae
zackees <z@zackees.com>
2023-01-13 01:49:20 -0800

    encoding nit

--- test_install/run.py
+++ test_install/run.py
@@ -15,6 +15,7 @@ def print_env() -> None:
     for path in paths:
         print(f"  {path}")
 
+import chardet
 
 def main() -> int:
     print_env()
@@ -29,12 +30,14 @@ def main() -> int:
     print("STDOUT:")
     for line in proc.stdout:
         # Have to use print() in order to get the output to the console in order.
-        line = line.decode('utf-8', errors='replace')
+        enc = chardet.detect(line)
+        line = line.decode(enc, errors='replace')
         print(line, end="")
     print("STDERR:")
     for line in proc.stderr:
         # Have to use print() in order to get the output to the console in order.
-        line = line.decode('utf-8', errors='replace')
+        enc = chardet.detect(line)
+        line = line.decode(enc, errors='replace')
         print(line, end="")
     rtn = proc.wait()
     print("\n\n\n")
