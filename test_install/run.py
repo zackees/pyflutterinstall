5bd0faab804221d596520b1dfe936728bdbe6d24
zackees <z@zackees.com>
2023-01-13 01:30:25 -0800

    try and fix encoding

--- test_install/run.py
+++ test_install/run.py
@@ -19,19 +19,21 @@ def print_env() -> None:
 def main() -> int:
     print_env()
     print("\nstarting pyflutterinstall...")
+    env = os.environ.copy()
+    env['OutputEncoding'] = 'utf8'
     proc = subprocess.Popen(
         "pyflutterinstall --skip-confirmation --skip-chrome",
         shell=True,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        env=env,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        encoding="utf-8")
     print("STDOUT:")
     for line in proc.stdout:
         # Have to use print() in order to get the output to the console in order.
-        line = line.decode("cp1252")
         print(line, end="")
     print("STDERR:")
     for line in proc.stderr:
         # Have to use print() in order to get the output to the console in order.
-        line = line.decode("cp1252")
         print(line, end="")
     rtn = proc.wait()
     print("\n\n\n")
