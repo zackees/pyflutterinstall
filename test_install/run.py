277a4b5a981baec1da2d5550bf365c4bcb294458
zackees <z@zackees.com>
2023-01-13 01:18:48 -0800

    fix encoding issue

--- test_install/run.py
+++ test_install/run.py
@@ -22,15 +22,16 @@ def main() -> int:
     proc = subprocess.Popen(
         "pyflutterinstall --skip-confirmation --skip-chrome",
         shell=True,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        universal_newlines=True)
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print("STDOUT:")
     for line in proc.stdout:
         # Have to use print() in order to get the output to the console in order.
+        line = line.decode("cp1252")
         print(line, end="")
     print("STDERR:")
     for line in proc.stderr:
         # Have to use print() in order to get the output to the console in order.
+        line = line.decode("cp1252")
         print(line, end="")
     rtn = proc.wait()
     print("\n\n\n")
