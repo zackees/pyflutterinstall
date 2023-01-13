540be18a6c0590ec96b8142eecd1f6e42cdf470f
zackees <z@zackees.com>
2023-01-13 01:08:38 -0800

    improve integration test

--- test_install/run.py
+++ test_install/run.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 
 def print_env() -> None:
@@ -18,7 +19,20 @@ def print_env() -> None:
 def main() -> int:
     print_env()
     print("\nstarting pyflutterinstall...")
-    rtn = os.system("pyflutterinstall --skip-confirmation --skip-chrome")
+    proc = subprocess.Popen(
+        "pyflutterinstall --skip-confirmation --skip-chrome",
+        shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        universal_newlines=True)
+    print("STDOUT:")
+    for line in proc.stdout:
+        # Have to use print() in order to get the output to the console in order.
+        print(line, end="")
+    print("STDERR:")
+    for line in proc.stderr:
+        # Have to use print() in order to get the output to the console in order.
+        print(line, end="")
+    rtn = proc.wait()
     print("\n\n\n")
     print_env()
     return rtn
