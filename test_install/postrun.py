7ad701eaf24f896393f67481c6be8b365518d07c
zackees <z@zackees.com>
2023-01-13 23:28:44 -0800

    try fix

--- test_install/postrun.py
+++ test_install/postrun.py
@@ -7,16 +7,18 @@ Pre run script
 import os
 import sys
 from shutil import which
+
 # import win32api
 
 from colorama import just_fix_windows_console  # type: ignore
 
 from pyflutterinstall.mywin32 import get_env_path
 
+
 def main() -> int:
     """Checks the environment and other tools are correct before run is invoked."""
     just_fix_windows_console()  # Fixes color breakages
-    #current_path = win32api.GetEnvironmentVariable("PATH")
+    # current_path = win32api.GetEnvironmentVariable("PATH")
     current_path = get_env_path()
     os.environ.update({"PATH": current_path})
     # Print out the current environment
