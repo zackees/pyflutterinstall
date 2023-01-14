f45e291d15080748360539d064580b50fa8359ae
zackees <z@zackees.com>
2023-01-13 20:22:21 -0800

    improve setting the environment by using powershell and setx

--- pyflutterinstall/envset_win32.py
+++ pyflutterinstall/envset_win32.py
@@ -9,37 +9,18 @@ import os
 from pathlib import Path
 
 
-def broadcast_env_change():
-    if sys.platform == "win32":
-        import win32con  # type: ignore
-        import win32gui  # type: ignore
-
-        SendMessageTimeout = win32gui.SendMessageTimeout  # type: ignore
-        SendMessageTimeout(
-            win32con.HWND_BROADCAST,
-            win32con.WM_SETTINGCHANGE,
-            0,
-            "Environment",
-            win32con.SMTO_ABORTIFHUNG,
-            5000,
-        )
-    else:
-        raise OSError(f"Unsupported platform: {sys.platform}")
-
+def gen_powershell_script(path: Path):
+    old_path = os.environ["PATH"]
+    new_path = f"{str(path)};{old_path}"
+    cmd = f"""[Environment]::SetEnvironmentVariable('PATH', '{new_path}','User');"""
+    return cmd
 
 def add_system_path(new_path: Path):
     if sys.platform == "win32":
-        import windowspathadder.windowspathadder  # type: ignore
-
         print(f"Adding {new_path} to Windows PATH")
-
-        def null_print(*args, **kwargs):
-            pass
-
-        windowspathadder.windowspathadder._print = null_print
-        windowspathadder.add_windows_path(str(new_path))
+        cmd = gen_powershell_script(new_path)
+        os.system(cmd)
         os.environ["PATH"] += os.pathsep + str(new_path)
-        broadcast_env_change()
     else:
         raise OSError(f"Unsupported platform: {sys.platform}")
 
@@ -49,14 +30,6 @@ def set_env_var(var_name, var_value):
     var_name = str(var_name)
     var_value = str(var_value)
     if sys.platform == "win32":
-        import winreg
-
-        print(f"Setting {var_name} to {var_value}")
-        key = winreg.OpenKey(
-            winreg.HKEY_CURRENT_USER, "Environment", 0, winreg.KEY_ALL_ACCESS
-        )
-        winreg.SetValueEx(key, var_name, 0, winreg.REG_SZ, var_value)
-        winreg.CloseKey(key)
-        broadcast_env_change()
+        os.system(f'setx {var_name} "{var_value}"')
     else:
         raise OSError(f"Unsupported platform: {sys.platform}")
