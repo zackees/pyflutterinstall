b07d53d704729f273fb220ed1745f05365495f0f
zackees <zachvorhies@protonmail.com>
2023-01-21 16:44:00 -0500

    move stderr to threaad

--- pyflutterinstall/install_flutter_sdk.py
+++ pyflutterinstall/install_flutter_sdk.py
@@ -57,11 +57,12 @@ def install_flutter_sdk() -> None:
         ignore_errors=False,
     )
 
-    execute(
-        "flutter doctor --android-licenses",
-        send_confirmation="y\ny\ny\n",
-        ignore_errors=False,
-    )
+    #execute(
+    #    "flutter doctor --android-licenses",
+    #    send_confirmation="y\ny\ny\n",
+    #    ignore_errors=False,
+    #)
+    os.system("echo y | flutter doctor --android-licenses")
     print("Flutter installed.\n")
 
 
