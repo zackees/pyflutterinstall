6a95507010eece1700bed507abfbc4130381178a
zackees <zachvorhies@protonmail.com>
2023-04-25 11:42:28 -0700

    fix java trampoline for windows

--- pyflutterinstall/cmds/java.py
+++ pyflutterinstall/cmds/java.py
@@ -23,12 +23,19 @@ def find_default_path_or_none() -> str | None:
     if len(jdk_folders) > 1:
         jdk_folders.sort()
         jdk_folders.reverse()
-    return os.path.join(JAVA_DIR, jdk_folders[0], "bin")
+    base_java_dir = os.path.join(JAVA_DIR, jdk_folders[0])
+    if sys.platform == "darwin":
+        java_bin = os.path.join(base_java_dir, "Contents", "Home", "bin")
+    else:
+        java_bin = os.path.join(base_java_dir, "bin")
+    return java_bin
 
 
 def main(argv: list[str] | None = None) -> int:
     """Main"""
-    return trampoline(COMMAND, args=argv, default_path=find_default_path_or_none())
+    java_bin = trampoline(COMMAND, args=argv, default_path=find_default_path_or_none())
+    assert os.path.exists(java_bin), f"java_bin {java_bin} does not exist"
+    return 0
 
 
 if __name__ == "__main__":
