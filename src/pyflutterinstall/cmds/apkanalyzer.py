69b91e96b11e83f04878d02e4629c8977ccd1933
zackees <z@zackees.com>
2023-10-03 21:47:31 -0700

    try and fix ubuntu

--- src/pyflutterinstall/cmds/apkanalyzer.py
+++ src/pyflutterinstall/cmds/apkanalyzer.py
@@ -8,7 +8,8 @@ import sys
 from pyflutterinstall.trampoline import trampoline
 
 from pyflutterinstall.config import config_load
-from pyflutterinstall.util import print_tree_dir
+
+# from pyflutterinstall.util import print_tree_dir
 
 android_sdk = config_load().get("ANDROID_SDK", ".")
 if android_sdk != ".":
@@ -19,39 +20,13 @@ if android_sdk != ".":
 COMMAND = "apkanalyzer"
 if sys.platform == "win32":
     COMMAND += ".bat"
-DEFAULT_PATH = os.path.join(
-    android_sdk, "cmdline-tools", "tools", "cmdline-tools", "bin"
-)
-LATEST_PATH = os.path.join(android_sdk, "cmdline-tools", "latest", "bin")
 
-BINARY_PATHS = [
-    DEFAULT_PATH,
-    LATEST_PATH,
-]
-
-
-def find_path() -> str:
-    """Find path"""
-    for path in BINARY_PATHS:
-        final_path = os.path.join(path, COMMAND)
-        if sys.platform == "win32":
-            final_path += ".bat"
-        if os.path.exists(path):
-            return path
-    raise FileNotFoundError(
-        f"Could not find {COMMAND} on any of the paths: {BINARY_PATHS}"
-    )
+DEFAULT_PATH = os.path.join(android_sdk, "cmdline-tools", "latest", "bin")
 
 
 def main(argv: list[str] | None = None) -> int:
     """Main"""
-    try:
-        default_path = find_path()
-    except FileNotFoundError as exc:
-        print(exc)
-        print_tree_dir(os.environ["ANDROID_HOME"])
-        return 1
-    return trampoline(COMMAND, args=argv, default_path=default_path)
+    return trampoline(COMMAND, args=argv, default_path=DEFAULT_PATH)
 
 
 if __name__ == "__main__":
