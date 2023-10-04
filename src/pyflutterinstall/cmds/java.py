f869331ee267766b41fa12198eba22b03140f080
zackees <z@zackees.com>
2023-10-04 01:12:44 -0700

    more debugging

--- src/pyflutterinstall/cmds/java.py
+++ src/pyflutterinstall/cmds/java.py
@@ -35,6 +35,7 @@ def find_default_path_or_none() -> str | None:
 def main(argv: list[str] | None = None) -> int:
     """Main"""
     print(f"Java dir: {JAVA_DIR}")
+    print(os.listdir(JAVA_DIR))
     rtn = trampoline(COMMAND, args=argv, default_path=find_default_path_or_none())
     return rtn
 
