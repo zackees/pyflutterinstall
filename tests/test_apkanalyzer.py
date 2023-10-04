e02e12fb77fa9d881245c4b98d7b8f759b9df2b2
zackees <z@zackees.com>
2023-10-03 19:01:52 -0700

    update debugging on test

--- tests/test_apkanalyzer.py
+++ tests/test_apkanalyzer.py
@@ -5,6 +5,8 @@ Unit test file.
 import unittest
 
 import os
+import subprocess
+import shutil
 from pyflutterinstall.config import config_load
 
 config = config_load()
@@ -18,6 +20,10 @@ class JavaTest(unittest.TestCase):
     def test_java(self) -> None:
         """Tests that we can bind to the java executable."""
         print("Test apkanalyzer")
+        java_version = subprocess.check_output("java -version", shell=True, universal_newlines=True, stderr=subprocess.STDOUT)
+        print(f"Java version: {java_version}\n")
+        java_path = shutil.which("java")
+        print(f"Java path: {java_path}\n")
         rtn = os.system("apkanalyzer --help")
         self.assertEqual(rtn, 0)
 
