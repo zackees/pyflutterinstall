16e14e22b079e55dd2e16f8db66086cd510edf81
zackees <z@zackees.com>
2023-10-04 17:43:01 -0700

    try and fix macos

--- src/pyflutterinstall/cmds/java.py
+++ src/pyflutterinstall/cmds/java.py
@@ -20,26 +20,29 @@ def find_java_exe(java_base_dir: str) -> str | None:
     for root, dirs, files in os.walk(java_base_dir):
         if "bin" in dirs:
             bin_dir = os.path.join(root, "bin")
-            java_bin = os.path.join(bin_dir, "java")
+            java_exe = os.path.join(bin_dir, "java")
             if sys.platform == "win32":
-                java_bin += ".exe"
-            if os.path.isfile(java_bin):
-                return java_bin
+                java_exe += ".exe"
+            if os.path.isfile(java_exe):
+                return java_exe
     return None
 
 
 def find_default_path_or_none() -> str | None:
     """Find default path"""
+    java_dir = JAVA_DIR
     if JAVA_DIR is None:
+        java_dir = os.environ.get("JAVA_HOME", None)
+    if java_dir is None:
         return None
-    jdk_folders = os.listdir(JAVA_DIR)
+    jdk_folders = os.listdir(java_dir)
     if not jdk_folders:
         return None
     if len(jdk_folders) > 1:
         jdk_folders.sort()
         jdk_folders.reverse()
     print(f"jdk_folders: {jdk_folders}")
-    base_java_dir = os.path.join(JAVA_DIR, jdk_folders[0])
+    base_java_dir = os.path.join(java_dir, jdk_folders[0])
     if "linux" in sys.platform:
         return base_java_dir
     if "darwin" in sys.platform:
@@ -57,9 +60,10 @@ def main(argv: list[str] | None = None) -> int:
     #print_tree_dir(JAVA_DIR, max_level=5)
     java_home = find_default_path_or_none()
     java_exe = find_java_exe(java_home)
-    print(f"Java bin: {java_exe}")
+    print(f"Java home: {java_home}")
+    print(f"Java exe: {java_exe}")
     os.environ["JAVA_HOME"] = java_home
-    print(f"Searched for bin directory and found: {find_java_exe(java_home)}")
+    print(f"Searched for bin directory and found: {java_exe}")
     print(f"JAVA_HOME: {java_home}")
     rtn = trampoline(COMMAND, args=argv, default_path=java_exe)
     return rtn
