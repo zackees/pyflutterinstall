f68256b486b849974a38ee7760b203f97d2734e1
zackees <z@zackees.com>
2023-10-04 23:52:11 -0700

    more debugging

--- test_install/is_java_version.py
+++ test_install/is_java_version.py
@@ -32,12 +32,10 @@ def main() -> int:
     parser.add_argument("version", type=str, help="java version to check for")
     args = parser.parse_args()
     java_version = get_java_version()
-    # print environment
-    for key, val in sorted(os.environ.items()):
-        print(f"{key} = {val}")
     if args.version not in java_version:
         print(f"Java version {args.version} not found")
         print("Printing environment")
+        print(f"which java: {which('java')}")
         env_copy = os.environ.copy()
         paths = env_copy.pop("PATH")
         for key, val in sorted(env_copy.items()):
