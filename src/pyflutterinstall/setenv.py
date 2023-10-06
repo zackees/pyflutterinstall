e3af81a363ef629c2aceda915b1c6fbf79fb9d1b
zackees <z@zackees.com>
2023-10-06 00:04:05 -0700

    fix

--- src/pyflutterinstall/setenv.py
+++ src/pyflutterinstall/setenv.py
@@ -29,7 +29,7 @@ def set_env_var(var_name: str, var_value: Union[str, Path], verbose=True):
     if verbose:
         print(f"$$$ Setting {var_name} to {var_value}")
     config = config_load()
-    env = config.setdefault("PATH", {})
+    env = config.setdefault("ENV", {})
     env[var_name] = var_value
     config_save(config)
     setenvironment.set_env_var(var_name, var_value)
