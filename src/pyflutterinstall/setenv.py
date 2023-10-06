7eac20e28d41fc86b10a54914ed23072a4667580
zackees <z@zackees.com>
2023-10-05 23:42:14 -0700

    try and fix ubuntu

--- src/pyflutterinstall/setenv.py
+++ src/pyflutterinstall/setenv.py
@@ -9,6 +9,8 @@ from typing import Union
 
 import setenvironment  # type: ignore
 
+from pyflutterinstall.config import config_load, config_save
+
 
 def uniquify_paths(paths: list[str]) -> list[str]:
     """Removes duplicate paths from the given path string."""
@@ -26,11 +28,17 @@ def set_env_var(var_name: str, var_value: Union[str, Path], verbose=True):
     var_value = str(var_value)
     if verbose:
         print(f"$$$ Setting {var_name} to {var_value}")
+    config = config_load()
+    config["env"][var_name] = var_value
+    config_save(config)
     setenvironment.set_env_var(var_name, var_value)
 
 
 def add_env_path(new_path: Union[Path, str]):
     """Adds a path to the front of the PATH environment variable."""
+    config = config_load()
+    config["PATH"] = uniquify_paths([str(new_path)] + config["env"]["PATH"].split(":"))
+    config_save(config)
     setenvironment.add_env_path(new_path)
 
 
@@ -46,9 +54,19 @@ def get_env_var(var_name: str) -> str:
 
 def unset_env_var(var_name: str) -> str:
     """Unsets an environment variable."""
+    config = config_load()
+    config["env"].pop(var_name, None)
+    config_save(config)
     return setenvironment.unset_env_var(var_name)
 
 
 def remove_env_path(path: Union[Path, str]) -> None:
     """Removes a path from the PATH environment variable."""
-    setenvironment.remove_env_path(str(path))
+    config = config_load()
+    path = str(path)
+    paths = config["PATH"]
+    while path in paths:
+        paths.remove(path)
+    config["PATH"] = paths
+    config_save(config)
+    setenvironment.remove_env_path(path)
