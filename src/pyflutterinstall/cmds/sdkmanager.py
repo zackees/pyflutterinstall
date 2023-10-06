afa6b4c7e0d4b5e9ea1f7e60acbf1b6dadecb5cd
zackees <z@zackees.com>
2023-10-05 23:22:55 -0700

    try and fix sdkmanager

--- src/pyflutterinstall/cmds/sdkmanager.py
+++ src/pyflutterinstall/cmds/sdkmanager.py
@@ -8,14 +8,14 @@ from pyflutterinstall.config import config_load
 
 from pyflutterinstall.cmds.java import find_default_path_or_none
 
-ANDROID_SDK = config_load().get("ANDROID_SDK", ".")
-JAVA_DIR = config_load().get("JAVA_DIR", None)
-if ANDROID_SDK != ".":
-    os.environ["ANDROID_SDK"] = ANDROID_SDK
-    os.environ["ANDROID_HOME"] = ANDROID_SDK
+from pyflutterinstall.paths import Paths
+
+paths = Paths()
+paths.apply_env()
+
 
 COMMAND = "sdkmanager"
-DEFAULT_PATH = os.path.join(ANDROID_SDK, "cmdline-tools", "latest", "bin")
+DEFAULT_PATH = os.path.join(paths.ANDROID_SDK, "cmdline-tools", "latest", "bin")
 
 
 def main(argv: list[str] | None = None) -> int:
@@ -27,7 +27,7 @@ def main(argv: list[str] | None = None) -> int:
             has_sdk_root = True
             break
     if not has_sdk_root:
-        argv.append(f"--sdk_root={ANDROID_SDK}")
+        argv.append(f"--sdk_root={paths.ANDROID_SDK}")
     java_home = find_default_path_or_none()
     assert java_home is not None, "Java home not found"
     os.environ["JAVA_HOME"] = java_home
